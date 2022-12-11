ece04b256d88d4fb3d4470afee9adb459abb4916
Ian Weiss <isweiss93@gmail.com>
2022-12-11 14:09:00 -0500

    clean up

--- 2022/src/main/scala/Day11.scala
+++ 2022/src/main/scala/Day11.scala
@@ -16,44 +16,29 @@ object Day11 {
       .toList
       .map(_.map(_.filter(c => c.isDigit || c == '*' || c == '+' || c == ',')))
       .map { m =>
-        val id: Int                  = m.head.toInt
-        val items: List[Long]        = m(1).split(",").toList.map(_.toLong)
         val op: (Long, Long) => Long = if (m(2).head == '*') _ * _ else _ + _
-        val mod: Option[Long]        = m(2).tail.toLongOption
-        val div: Long                = m(3).toLong
-        val yes: Int                 = m(4).toInt
-        val no: Int                  = m(5).toInt
-        (
-          id,
-          Monkey(
-            id           = id,
-            items        = items,
-            div          = div,
-            op           = (w: Long) => op(w, mod.getOrElse(w)),
-            test         = (i: Long) => if (i % div == 0) yes else no,
-            numInspected = 0
-          )
+        Monkey(
+          id           = m.head.toInt,
+          items        = m(1).split(",").toList.map(_.toLong),
+          op           = (w: Long) => op(w, m(2).tail.toLongOption.getOrElse(w)),
+          div          = m(3).toLong,
+          idIfTrue     = m(4).toInt,
+          idIfFalse    = m(5).toInt,
+          numInspected = 0
         )
       }
-      .toMap
 
-    val rounds20 = 1.to(20).foldLeft(monkeys) { case (m, _) =>
-      m.keys.toList.sorted.foldLeft(m) { case (map, id) =>
-        map(id).takeTurn(map, _ / 3)
-      }
-    }
+    val rounds20    = observe(monkeys, 20, _ / 3)
+    val rounds10000 = observe(monkeys, 10000, _ % monkeys.map(_.div).product)
 
-    val scaleDown = monkeys.values.map(_.div).product
-
-    val rounds10000 = 1.to(10000).foldLeft(monkeys) { case (m, _) =>
-      m.keys.toList.sorted.foldLeft(m) { case (map, id) =>
-        map(id).takeTurn(map, _ % scaleDown)
-      }
-    }
-
-    println(rounds20.values.toList.map(_.numInspected).sorted.takeRight(2).product)
-    println(rounds10000.values.toList.map(_.numInspected).sorted.takeRight(2).product)
+    println(rounds20.map(_.numInspected).sorted.takeRight(2).product)
+    println(rounds10000.map(_.numInspected).sorted.takeRight(2).product)
   }
+
+  private def observe(monkeys: List[Monkey], rounds: Int, scaleDown: Long => Long): List[Monkey] =
+    1.to(rounds).foldLeft(monkeys) { case (m, _) =>
+      m.foldLeft(m) { case (list, monkey) => list(monkey.id).takeTurn(list, scaleDown) }
+    }
 }
 
 case class Monkey(
@@ -61,17 +46,18 @@ case class Monkey(
   items: List[Long],
   op: Long => Long,
   div: Long,
-  test: Long => Int,
+  idIfTrue: Int,
+  idIfFalse: Int,
   numInspected: Long
 ) {
 
-  def takeTurn(monkeys: Map[Int, Monkey], scaleDown: Long => Long): Map[Int, Monkey] = {
-    val map = items.foldLeft(monkeys) { case (map, item) =>
-      val worry  = scaleDown(op(item))
-      val target = test(worry)
-      val monkey = map(target)
-      map.updated(target, monkey.copy(items = monkey.items :+ worry))
-    }
-    map.updated(id, this.copy(items = List.empty, numInspected = numInspected + items.length))
+  def takeTurn(monkeys: List[Monkey], scaleDown: Long => Long): List[Monkey] = {
+    items
+      .foldLeft(monkeys) { case (list, item) =>
+        val worry  = scaleDown(op(item))
+        val target = if (worry % div == 0) idIfTrue else idIfFalse
+        list.updated(target, list(target).copy(items = list(target).items :+ worry))
+      }
+      .updated(id, this.copy(items = List.empty, numInspected = numInspected + items.length))
   }
 }
